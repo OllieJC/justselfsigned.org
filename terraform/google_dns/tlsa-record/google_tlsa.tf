2d09cecf47ad484b4535d3ec7c09c8077552fa95
OllieJC <5426038+OllieJC@users.noreply.github.com>
2022-12-02 23:17:56 +0000

    Update google_tlsa.tf

--- terraform/google_dns/tlsa-record/google_tlsa.tf
+++ terraform/google_dns/tlsa-record/google_tlsa.tf
@@ -14,6 +14,22 @@ resource "google_dns_record_set" "udp-tlsa-record" {
   rrdatas = var.cert_sig
 }
 
+resource "google_dns_record_set" "www-tlsa-cname-record" {
+  managed_zone = replace(var.domain, ".", "-")
+  name    = "_443._tcp.www.${var.domain}."
+  type    = "CNAME"
+  ttl     = "30"
+  rrdatas = ["_443._tcp.${var.domain}."]
+}
+
+resource "google_dns_record_set" "www-udp-tlsa-cname-record" {
+  managed_zone = replace(var.domain, ".", "-")
+  name    = "_443._udp.www.${var.domain}."
+  type    = "CNAME"
+  ttl     = "30"
+  rrdatas = ["_443._tcp.${var.domain}."]
+}
+
 resource "google_dns_record_set" "precert-tlsa-record" {
   count        = var.enable_precert_sign ? 1 : 0
   managed_zone = replace(var.domain, ".", "-")
